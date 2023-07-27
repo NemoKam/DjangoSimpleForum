460826062409c9aad22ca8a316acb8a85ffee769
NemoKam <kamkamuch.7@gmail.com>
2023-07-27 22:35:55 +0300

    added middleware in settings.py

--- forum/forum/settings.py
+++ forum/forum/settings.py
@@ -45,6 +45,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    #apps middleware
+    'authapp.middleware.ip_statistic',
 ]
 
 ROOT_URLCONF = 'forum.urls'
