ce5a17c4b85ed1870e64166da61493adf6fded21
NemoKam <kamkamuch.7@gmail.com>
2023-07-27 22:34:56 +0300

    added new model in admin.py

--- forum/authapp/admin.py
+++ forum/authapp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from authapp.models import UserRequest
+
+class UserRequestAdmin(admin.ModelAdmin):
+    list_display = ['id', 'ip', 'request_count', 'request_started']
+    ordering = ['id']
+    list_display_links = ['ip']
+
+
+admin.site.register(UserRequest, UserRequestAdmin)
